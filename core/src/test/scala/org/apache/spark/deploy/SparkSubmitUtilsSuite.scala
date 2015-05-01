9e5a2ef02aced5ebdb188737de93b67a309b7cde
Patrick Wendell <patrick@databricks.com>
2015-05-01 14:42:58 -0700

    Ignore flakey test in SparkSubmitUtilsSuite

--- core/src/test/scala/org/apache/spark/deploy/SparkSubmitUtilsSuite.scala
+++ core/src/test/scala/org/apache/spark/deploy/SparkSubmitUtilsSuite.scala
@@ -191,7 +191,7 @@ class SparkSubmitUtilsSuite extends SparkFunSuite with BeforeAndAfterAll {
     }
   }
 
-  test("neglects Spark and Spark's dependencies") {
+  ignore("neglects Spark and Spark's dependencies") {
     val components = Seq("bagel_", "catalyst_", "core_", "graphx_", "hive_", "mllib_", "repl_",
       "sql_", "streaming_", "yarn_", "network-common_", "network-shuffle_", "network-yarn_")
 
